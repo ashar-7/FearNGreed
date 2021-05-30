ea78bdd5b61240a8093202b2eecf6dd8c0995411
ashar-7 <asharalikhan200@gmail.com>
2021-05-30 21:20:29 +0530

    Remove Scaffold from CurrentIndexScreen.kt temporarily

--- app/src/main/java/com/se7en/fearngreed/ui/index/CurrentIndexScreen.kt
+++ app/src/main/java/com/se7en/fearngreed/ui/index/CurrentIndexScreen.kt
@@ -1,15 +1,8 @@
 package com.se7en.fearngreed.ui.index
 
-import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 
 @Composable
 fun CurrentIndexScreen(viewModel: CurrentIndexViewModel) {
-    Scaffold(
-        topBar = {
 
-        }
-    ) {
-
-    }
 }
